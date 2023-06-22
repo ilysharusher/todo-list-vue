49cc1590b1534ea8f45936afd76835af8c1c2ec6
ilysharusher <illyaetoilysha@gmail.com>
2023-06-22 19:09:45 +0300

    Setting up Axios for HTTP call & install prettier

---
+++ src/http/task-api.js
@@ -0,0 +1,13 @@
+import api from '@/http/api';
+
+const resource = '/tasks';
+
+export const getTasks = () => api.get(resource);
+
+export const createTask = (task) => api.post(resource, task);
+
+export const updateTask = (id, task) => api.put(`${resource}/${id}`, task);
+
+export const deleteTask = (id) => api.delete(`${resource}/${id}`);
+
+export const completeTask = (id) => api.patch(`${resource}/${id}/complete`);
