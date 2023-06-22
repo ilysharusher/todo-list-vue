31098f1cfa72e9a57d20dc42f11738f38262aed4
ilysharusher <illyaetoilysha@gmail.com>
2023-06-22 21:40:17 +0300

    Displaying data in Vue js components

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <script setup>
-import Navbar from "@/components/Navbar.vue";
-import TasksPage from "@/pages/TasksPage.vue";
-import Footer from "@/components/Footer.vue";
+import Navbar from '@/components/Navbar.vue';
+import TasksPage from '@/pages/TasksPage.vue';
+import Footer from '@/components/Footer.vue';
 </script>
 
 <template>
-  <Navbar/>
-  <TasksPage/>
-  <Footer/>
+    <Navbar />
+    <TasksPage />
+    <Footer />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
---
+++ src/components/tasks/Task.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { defineProps } from 'vue';
+import IconPencil from '@/components/icons/IconPencil.vue';
+import IconTrash from '@/components/icons/IconTrash.vue';
+
+const props = defineProps({
+    task: {
+        type: Object,
+        required: true
+    }
+});
+</script>
+
+<template>
+    <li class="list-group-item py-3">
+        <div class="d-flex justify-content-start align-items-center">
+            <input class="form-check-input mt-0 completed" type="checkbox" />
+            <div class="ms-2 flex-grow-1" title="Double click the text to edit or remove">
+                <!-- <div class="relative">
+                    <input class="editable-task" type="text" />
+                </div> -->
+                <span>{{ props.task.name }}</span>
+            </div>
+            <!--                                    <div class="task-date">24 Feb 12:00</div>-->
+        </div>
+        <div class="task-actions">
+            <button class="btn btn-sm btn-circle btn-outline-secondary me-1">
+                <IconPencil />
+            </button>
+            <button class="btn btn-sm btn-circle btn-outline-danger">
+                <IconTrash />
+            </button>
+        </div>
+    </li>
+</template>
+
+<style scoped></style>
