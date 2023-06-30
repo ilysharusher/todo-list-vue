bcbe301eb168f5062f602b39ecc8ce38728b186a
ilysharusher <illyaetoilysha@gmail.com>
2023-06-30 13:36:15 +0300

    Installing Pinia

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import './assets/main.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/main.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import {createApp} from 'vue'
-import App from './App.vue'
+import { createPinia } from 'pinia';
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(createPinia());
+app.mount('#app');
