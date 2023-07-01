e3fe16399725bf737c82ca22bd700defe3fd9ba7
ilysharusher <illyaetoilysha@gmail.com>
2023-07-01 12:08:15 +0300

    Refactoring Stores (old code deleted)

--- src/stores/task.js
+++ src/stores/task.js
@@ -2,26 +2,6 @@ import { defineStore } from 'pinia';
 import { getTasks } from '@/http/task-api';
 import { ref, computed } from 'vue';
 
-/*const old = {
-    state: () => ({
-        tasks: []
-    }),
-    getters: {
-        completedTasks() {
-            return this.tasks.filter((task) => task.is_completed);
-        },
-        uncompletedTasks() {
-            return this.tasks.filter((task) => !task.is_completed);
-        }
-    },
-    actions: {
-        async fetchTasks() {
-            const { data } = await getTasks();
-            this.tasks = data.data;
-        }
-    }
-};*/
-
 export const useTaskStore = defineStore('taskStore', () => {
     const tasks = ref([]);
 
