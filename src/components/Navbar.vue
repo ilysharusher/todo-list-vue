194520ca1c89c7f6ab556ec835f9f919c2d3e33d
ilysharusher <illyaetoilysha@gmail.com>
2023-07-03 22:28:36 +0300

    Added Route Navigation

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,43 +1,50 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-    <div class="container py-2">
-      <a href="#" class="navbar-brand">
-        <span>ToDo</span>
-        <strong>List</strong>
-      </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-              aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a href="my-tasks.html" class="nav-link">Tasks</a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">Summary</a>
-          </li>
-        </ul>
-        <ul class="navbar-nav ms-auto">
-          <li class="nav-item">
-            <a href="#" class="btn btn-outline-secondary ms-2">Login</a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="btn btn-danger ms-2">Register</a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="btn btn-outline-secondary ms-2">Logout</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+        <div class="container py-2">
+            <router-link :to="{ name: 'tasks' }" class="navbar-brand">
+                <span>ToDo</span>
+                <strong>List</strong>
+            </router-link>
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <router-link :to="{ name: 'tasks' }" class="nav-link">Tasks</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Summary</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <router-link :to="{ name: 'login' }" class="btn btn-outline-secondary ms-2"
+                            >Login
+                        </router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link :to="{ name: 'signup' }" class="btn btn-danger ms-2"
+                            >Register
+                        </router-link>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="btn btn-outline-secondary ms-2">Logout</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
