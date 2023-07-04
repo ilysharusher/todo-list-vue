34e110782ec608479eb5ea353c5f94c817632c00
ilysharusher <illyaetoilysha@gmail.com>
2023-07-04 14:26:27 +0300

    Programmatic navigations

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,8 +1,17 @@
-<script setup></script>
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const handleLogin = () => {
+    router.push({ name: 'tasks' });
+};
+</script>
 
 <template>
     <div>
         <h1>Login Page</h1>
+        <button @click="handleLogin" class="btn btn-primary">Login</button>
     </div>
 </template>
 
