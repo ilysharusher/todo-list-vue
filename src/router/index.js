447df34ec42a16d3fbad6cdffb1871e820c780a9
ilysharusher <illyaetoilysha@gmail.com>
2023-07-06 15:29:04 +0300

    Navigation Guards

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import routes from './routes';
 
-export default createRouter({
+const router = createRouter({
     routes,
     history: createWebHistory(),
     linkActiveClass: 'active'
 });
+
+router.beforeEach((to, from) => {
+    console.log(`${from.fullPath} => ${to.fullPath}`);
+});
+
+export default router;
